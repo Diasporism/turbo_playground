9ed4e80d87843e87d31facf2d287d53a4258eccf
Logan Sears <lsears322@gmail.com>
2024-12-05 18:50:55 -0700

    READY THE EXPIREMENT

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import DialogController from "./dialog_controller"
+application.register("dialog", DialogController)
